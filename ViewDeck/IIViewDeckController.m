1f344df3d00941e6b69bb60a938bd7ac34349f0e
Sean Woodhouse <sean@ittybittyapps.com>
2012-02-07 11:50:26 +1100

    Fixed compile warnings

--- ViewDeck/IIViewDeckController.m
+++ ViewDeck/IIViewDeckController.m
@@ -227,7 +227,8 @@
 }
 
 - (CGRect)slidingRectForOffset:(CGFloat)offset {
-    return (CGRect) { self.resizesCenterView && offset < 0 ? 0 : offset, 0, [self slidingSizeForOffset:offset] };
+    CGSize slidingSize = [self slidingSizeForOffset:offset];
+    return CGRectMake(self.resizesCenterView && offset < 0 ? 0 : offset, 0, slidingSize.width, slidingSize.height) ;
 }
 
 - (CGSize)slidingSizeForOffset:(CGFloat)offset {
@@ -435,8 +436,12 @@
     else {
         self.leftLedge = self.leftLedge + self.referenceBounds.size.width - _preRotationWidth; 
         self.rightLedge = self.rightLedge + self.referenceBounds.size.width - _preRotationWidth; 
-        self.leftController.view.frame = (CGRect) { self.leftController.view.frame.origin, _leftWidth, self.leftController.view.frame.size.height };
-        self.rightController.view.frame = (CGRect) { self.rightController.view.frame.origin, _rightWidth, self.rightController.view.frame.size.height };
+      
+        CGRect leftFrame = self.leftController.view.frame;
+        self.leftController.view.frame = CGRectMake(leftFrame.origin.x, leftFrame.origin.y, _leftWidth, leftFrame.size.height);
+      
+      CGRect rightFrame = self.rightController.view.frame;
+        self.rightController.view.frame = CGRectMake(rightFrame.origin.x, rightFrame.origin.y, _rightWidth, rightFrame.size.height);
     }
     self.slidingControllerView.frame = [self slidingRectForOffset:offset];
     
@@ -601,7 +606,7 @@
         [UIView animateWithDuration:CLOSE_SLIDE_DURATION(YES) delay:0 options:UIViewAnimationOptionCurveEaseOut | UIViewAnimationOptionLayoutSubviews animations:^{
             self.slidingControllerView.frame = [self slidingRectForOffset:0];
             [self centerViewVisible];
-        } completion:^(BOOL finished) {
+        } completion:^(BOOL finished2) {
             self.leftController.view.hidden = YES;
             if (completed) completed(self);
             [self performDelegate:@selector(viewDeckControllerDidCloseLeftView:animated:) animated:YES];
@@ -707,7 +712,7 @@
         [UIView animateWithDuration:CLOSE_SLIDE_DURATION(YES) delay:0 options:UIViewAnimationOptionCurveEaseOut | UIViewAnimationOptionLayoutSubviews animations:^{
             self.slidingControllerView.frame = [self slidingRectForOffset:0];
             [self centerViewVisible];
-        } completion:^(BOOL finished) {
+        } completion:^(BOOL finished2) {
             self.rightController.view.hidden = YES;
             if (completed) completed(self);
             [self performDelegate:@selector(viewDeckControllerDidCloseRightView:animated:) animated:YES];
